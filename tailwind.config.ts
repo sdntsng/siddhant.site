e7ed41076f1c020e75935a2320d913e6cc6e4682
sdntsng <s@siddhant.site>
2025-12-23 07:12:07 +0530

    feat: Add typography plugin configuration to lowercase headings.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,6 +76,15 @@ const config = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            "h1, h2, h3, h4": {
+              "text-transform": "lowercase",
+            },
+          },
+        },
+      },
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
