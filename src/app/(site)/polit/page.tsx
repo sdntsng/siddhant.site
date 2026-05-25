1a46dc197a6580ed670323568c9b70317f7570f1
sdntsng <s@siddhant.site>
2026-05-25 03:17:02 +0000

    Move Uttarakhand report to root public file; add /polit iframe page

---
+++ src/app/(site)/polit/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function PolitPage() {
+  return (
+    <div style={{ width: "100vw", height: "100vh", margin: 0, padding: 0 }}>
+      <iframe
+        src="/uttarakhand-report.html"
+        style={{
+          width: "100%",
+          height: "100%",
+          border: "none",
+        }}
+        title="Uttarakhand State Report"
+      />
+    </div>
+  );
+}
